1ef4e6df53549712a54cf3e382570060b2a26a9f
Siyanbola Akin <akin.siyanbola@gmail.com>
2022-02-10 14:49:32 +0100

    login complete

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+//splashscreen
 const splashscreentime = Duration(seconds: 3);
 
+//for colours
 const white = Colors.white;
+const textcolour = Color(0xff707070);
+const mainblue = Color(0xff44D9E6);
--- lib/views/loginscreen.dart
+++ lib/views/loginscreen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 import 'package:trko/res/constants.dart';
 import 'package:trko/res/size_config.dart';
 
@@ -12,11 +14,91 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
+            SizedBox(
+              height: getPropScreenHeight(97),
+            ),
             Image.asset(
               'assets/images/o.png',
               height: getPropScreenHeight(225),
               width: getPropScreenWidth(225),
               fit: BoxFit.contain,
+            ),
+            SizedBox(
+              width: getPropScreenWidth(337),
+              child: Text(
+                'Email',
+                style: TextStyle(
+                  color: textcolour,
+                  fontSize: getPropScreenWidth(20),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getPropScreenHeight(14),
+            ),
+            SizedBox(
+              height: getPropScreenHeight(60),
+              width: getPropScreenWidth(337),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: const InputDecoration(
+                  labelText: '',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.zero),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getPropScreenHeight(33),
+            ),
+            SizedBox(
+              width: getPropScreenWidth(337),
+              child: Text(
+                'Password',
+                style: TextStyle(
+                  color: textcolour,
+                  fontSize: getPropScreenWidth(20),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getPropScreenHeight(14),
+            ),
+            SizedBox(
+              height: getPropScreenHeight(60),
+              width: getPropScreenWidth(337),
+              child: TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: '',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.zero),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getPropScreenHeight(38),
+            ),
+            Container(
+              height: getPropScreenHeight(60),
+              width: getPropScreenWidth(337),
+              decoration: const BoxDecoration(
+                color: mainblue,
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: white,
+                    fontSize: getPropScreenWidth(20),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
